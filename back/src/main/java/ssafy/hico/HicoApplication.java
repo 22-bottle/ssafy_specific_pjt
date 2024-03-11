c1d287325d3f6712db6bfdd665ad054d0a459d46
손주현 <juhyeon042@naver.com>
2024-03-11 09:43:04 +0900

    init: 백엔드 환경세팅

---
+++ back/src/main/java/ssafy/hico/HicoApplication.java
@@ -0,0 +1,13 @@
+package ssafy.hico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HicoApplication.class, args);
+	}
+
+}
