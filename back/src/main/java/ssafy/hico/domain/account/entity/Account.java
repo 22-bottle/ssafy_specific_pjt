2526b15f9e885c98e2d69ce09aa1e050204feda2
Hgstudio <tndms0308@naver.com>
2024-03-12 11:55:28 +0900

    feat: Account Entity 생성

--- back/src/main/java/ssafy/hico/domain/account/entity/Account.java
+++ back/src/main/java/ssafy/hico/domain/account/entity/Account.java
@@ -1,4 +1,32 @@
 package ssafy.hico.domain.account.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import ssafy.hico.domain.member.entity.Member;
+import ssafy.hico.domain.member.entity.Parent;
+
+@Getter
+@Entity
+@Table(name = "account")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
+    @Column(name = "account")
+    private String account;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "bank_code")
+    private String bankCode;
+
+    @Column(name = "bank_name")
+    private String bankName;
 }
