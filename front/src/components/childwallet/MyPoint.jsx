fcaae2fcf3d2fcb61d0cb84cb107e24a2c09f41e
JIEUN LEE <jin3409975@gmail.com>
2024-03-12 12:55:23 +0900

    feat: 아이 AskWonComponent 레이아웃

--- front/src/components/childwallet/MyPoint.jsx
+++ front/src/components/childwallet/MyPoint.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Mypoint= () => {
+  
   const navigate = useNavigate();
 
   const askClick = () => {
@@ -13,25 +14,18 @@ const Mypoint= () => {
   const currencyButton = () => {
     navigate('/currency/detail')
   } 
+
+  const divBorder = {
+    border: "1px solid black",       
+    margin: "10px"
+}
     
   return (
-    <div 
-      style={{
-        border: "1px solid black",       
-        margin: "10px"
-      }}>
-      <div 
-        style={{
-          border: "1px solid black",
-          margin: "10px"
-        }}>
+    <div style={ divBorder }>
+      <div style={ divBorder }>
         <h2>획득 포인트</h2>
       </div>
-      <div 
-        style={{
-          border: "1px solid black",
-          margin: "10px"
-        }}>
+      <div style={ divBorder }>
         <h3>이승재님의 환전 가능 금액 총 20,226.07원</h3>
         <p>미국달러 보유 7.5달러 = 3,996.99원<button onClick={askClick}>버튼</button></p>
         <p>일본엔 보유 10.5엔 = 1,771.58원<button onClick={askClick}>버튼</button></p>
@@ -39,45 +33,21 @@ const Mypoint= () => {
         <p>중국위안 보유 0위안 = 0원<button onClick={askClick}>버튼</button></p>       
       </div>
       <p>실시간 환율 확인하기</p>
-      <div 
-        style={{
-          border: "1px solid black",
-          margin: "10px"
-        }}>
-        <div 
-          style={{
-          border: "1px solid black",
-          margin: "10px"
-        }}>
+      <div style={ divBorder }>
+        <div style={ divBorder }>
           <h2>실시간 환율</h2>
           <p>어제보다 오르락 내리락</p>
         </div>
-        <div
-          style={{
-            border: "1px solid black",
-            margin: "10px"
-          }}>
+        <div style={ divBorder }>
             미국달러 1,300원<button onClick={currencyButton}>버튼</button>
           </div>
-          <div
-          style={{
-            border: "1px solid black",
-            margin: "10px"
-          }}>
+          <div style={ divBorder }>
             일본엔 900원<button onClick={currencyButton}>버튼</button>
           </div>
-          <div
-          style={{
-            border: "1px solid black",
-            margin: "10px"
-          }}>
+          <div style={ divBorder }>
             유럽유로 1400원<button onClick={currencyButton}>버튼</button>
           </div>
-          <div
-          style={{
-            border: "1px solid black",
-            margin: "10px"
-          }}>
+          <div style={ divBorder }>
             중국위안 190원<button onClick={currencyButton}>버튼</button>
           </div>
       </div>
