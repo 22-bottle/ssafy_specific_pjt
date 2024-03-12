5ed8dfd6343f40483e48478bd4e763be4ca56a09
Hgstudio <tndms0308@naver.com>
2024-03-12 11:56:45 +0900

    feat: Parent Entity 수정

--- back/src/main/java/ssafy/hico/domain/member/entity/Parent.java
+++ back/src/main/java/ssafy/hico/domain/member/entity/Parent.java
@@ -1,4 +1,16 @@
 package ssafy.hico.domain.member.entity;
 
-public class Parent {
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "parent")
+@DiscriminatorValue("parent")
+public class Parent extends Member{
+    @Column(name = "invitation code")
+    private String invitationCode;
 }
