4032c5b24a88b4d97a096b08de673185b6695a4e
손주현 <juhyeon042@naver.com>
2024-03-13 16:51:39 +0900

    feat: 회원가입 시 계좌 유무 확인 로직 수정

--- back/src/main/java/ssafy/hico/domain/member/repository/MemberRepository.java
+++ back/src/main/java/ssafy/hico/domain/member/repository/MemberRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ssafy.hico.domain.member.entity.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    boolean findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
--- back/src/main/java/ssafy/hico/domain/member/service/MemberService.java
+++ back/src/main/java/ssafy/hico/domain/member/service/MemberService.java
@@ -4,13 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import ssafy.hico.domain.member.dto.request.BankMemberSearchRequest;
 import ssafy.hico.domain.member.dto.request.MemberSignUpRequest;
 import ssafy.hico.domain.member.dto.response.BankMemberSearchResponse;
 import ssafy.hico.domain.member.dto.response.MemberSignUpResponse;
 import ssafy.hico.domain.member.repository.MemberRepository;
 import ssafy.hico.global.bank.BankApi;
+import ssafy.hico.global.bank.BankApiClient;
 import ssafy.hico.global.bank.BankProperties;
 import ssafy.hico.global.response.error.ErrorCode;
 import ssafy.hico.global.response.error.exception.CustomException;
@@ -23,34 +23,27 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final BankProperties bankProperties;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final BankApiClient bankApiClient;
 
     public MemberSignUpResponse memberSignUp(MemberSignUpRequest request) {
-        if(memberRepository.findByEmail(request.getEmail())){
+        if(memberRepository.findByEmail(request.getEmail()).isPresent()){
             throw new CustomException(ErrorCode.DUPLICATE_EMAIL);
         }
 
         String memberSearchUrl = BankApi.MEMBER_SEARCH.getUrl();
         WebClient webClient = WebClient.create(memberSearchUrl);
-        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
-        parameters.add("userId", request.getEmail());
-        parameters.add("apiKey", bankProperties.getApiKey());
-
-        String response = webClient.post()
-                .uri(memberSearchUrl)
-                .bodyValue(parameters)
-                .retrieve()
-                .bodyToMono(String.class).block();
+        BankMemberSearchRequest requestBody = new BankMemberSearchRequest(request.getEmail(), bankProperties.getApiKey());
+        String response = bankApiClient.getResponse(memberSearchUrl, requestBody);
 
         ObjectMapper objectMapper = new ObjectMapper();
         BankMemberSearchResponse memberSearchResponse = null;
         try {
-            memberSearchResponse
-                    = objectMapper.readValue(response, BankMemberSearchResponse.class);
+            memberSearchResponse = objectMapper.readValue(response, BankMemberSearchResponse.class);
+
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
         String encryptPassword = bCryptPasswordEncoder.encode(request.getPassword());
-
 
         return MemberSignUpResponse.builder().name(request.getName()).build();
     }
