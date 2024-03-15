4964682aef739cce8167c98fd0459112e900f778
CHAEEUN LEE <cheeun0903@gmail.com>
2024-03-15 16:33:29 +0900

    feat: material ui 라디오 버튼

--- front/src/components/signup/Signup.jsx
+++ front/src/components/signup/Signup.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom'; 
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
 
 
 const Signup= () => {
@@ -17,88 +21,98 @@ const Signup= () => {
   };
     
   return (
-    <div style={ divBorder }>
-      <div style={ divBorder }>
-        <h1>회원가입<button>닫기</button></h1>
-      </div>
+    <FormControl>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+      </RadioGroup>
+    </FormControl>
+    // <div style={ divBorder }>
+    //   <div style={ divBorder }>
+    //     <h1>회원가입<button>닫기</button></h1>
+    //   </div>
       
-      {/* 아이 또는 부모 선택 */}
-      <div style={ divBorder }>
-        <label>
-          <input type="checkbox" name="" id="" />
-          아이
-        </label>
-        <label>
-          <input type="checkbox" name="" id="" />
-          부모
-        </label>       
-      </div>
+    //   {/* 아이 또는 부모 선택 */}
+    //   <div style={ divBorder }>
+    //     <label>
+    //       <input type="checkbox" name="" id="" />
+    //       아이
+    //     </label>
+    //     <label>
+    //       <input type="checkbox" name="" id="" />
+    //       부모
+    //     </label>       
+    //   </div>
 
-      {/* 부모 초대코드 입력 */}
-      <div style={ divBorder }>
-        <input 
-          type="text"
-          placeholder="초대코드를 입력해주세요." />
-      </div>
+    //   {/* 부모 초대코드 입력 */}
+    //   <div style={ divBorder }>
+    //     <input 
+    //       type="text"
+    //       placeholder="초대코드를 입력해주세요." />
+    //   </div>
 
-      {/* 성별 선택 */}
-      <div style={ divBorder }>
-        <select>
-          <option disabled selected>여성</option>
-          <option value="male">남성</option>
-        </select>
-      </div>
+    //   {/* 성별 선택 */}
+    //   <div style={ divBorder }>
+    //     <select>
+    //       <option disabled selected>여성</option>
+    //       <option value="male">남성</option>
+    //     </select>
+    //   </div>
 
-      {/* 생년월일 선택 */}
-      <div style={ divBorder }>
-        <select>
-          <option value="year">출생 연도</option>
-        </select>
-        <select>
-          <option value="month">월</option>
-        </select>
-        <select>
-          <option value="day">일</option>
-        </select>
-      </div>
+    //   {/* 생년월일 선택 */}
+    //   <div style={ divBorder }>
+    //     <select>
+    //       <option value="year">출생 연도</option>
+    //     </select>
+    //     <select>
+    //       <option value="month">월</option>
+    //     </select>
+    //     <select>
+    //       <option value="day">일</option>
+    //     </select>
+    //   </div>
 
-      {/* 이름 입력 */}
-      <div style={ divBorder }>
-        <input 
-          type="text"
-          placeholder="이름" />
-      </div>
+    //   {/* 이름 입력 */}
+    //   <div style={ divBorder }>
+    //     <input 
+    //       type="text"
+    //       placeholder="이름" />
+    //   </div>
 
-      {/* 이메일 입력 */}
-      <div style={ divBorder }>
-        <input 
-          type="text"
-          placeholder="이메일" />
-      </div>
+    //   {/* 이메일 입력 */}
+    //   <div style={ divBorder }>
+    //     <input 
+    //       type="text"
+    //       placeholder="이메일" />
+    //   </div>
 
-      {/* 비밀번호 입력 */}
-      <div style={ divBorder }>
-        <input 
-          type="text"
-          placeholder="비밀번호" />
-      </div>
+    //   {/* 비밀번호 입력 */}
+    //   <div style={ divBorder }>
+    //     <input 
+    //       type="text"
+    //       placeholder="비밀번호" />
+    //   </div>
 
-      {/* 비밀번호 확인 */}
-      <div style={ divBorder }>
-        <input 
-          type="text"
-          placeholder="비밀번호 확인" />
-      </div>
+    //   {/* 비밀번호 확인 */}
+    //   <div style={ divBorder }>
+    //     <input 
+    //       type="text"
+    //       placeholder="비밀번호 확인" />
+    //   </div>
 
-      {/* 회원가입 완료 버튼 */}
-      <div style={ divBorder }>
-        <button onClick={ completeClick }>완료</button>
-      </div>
+    //   {/* 회원가입 완료 버튼 */}
+    //   <div style={ divBorder }>
+    //     <button onClick={ completeClick }>완료</button>
+    //   </div>
 
 
       
 
-    </div>
+    // </div>
   );
 };
 
