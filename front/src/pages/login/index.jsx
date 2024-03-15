9e2cb55e055d5043ce0aa3937016168b9b0eeed7
CHAEEUN LEE <cheeun0903@gmail.com>
2024-03-15 10:41:26 +0900

    design: 스타일 디테일 작업

--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './login.module.css';
+import TextField from '@mui/material/TextField';
 
 function Login() {
   
@@ -7,13 +8,14 @@ function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // 로그인 처리 로직을 여기에 추가하세요.
-    // 예: 서버에 사용자 이름과 비밀번호를 전송하고 응답을 처리합니다.
-    console.log('로그인 시도:', username, password);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // 로그인 처리 로직을 여기에 추가하세요.
+  //   // 예: 서버에 사용자 이름과 비밀번호를 전송하고 응답을 처리합니다.
+  //   console.log('로그인 시도:', username, password);
+  // };
 
+  
   return (
       <div className={ styles.materialContainer }>
 
@@ -22,37 +24,47 @@ function Login() {
 
           <div className={ styles.title }>로그인</div>
 
-          <div className={ styles.input }>
-              <label htmlFor="username">이메일</label>
-              <input
+          <div>
+          <TextField
+            fullWidth
             id="username"
+            label="이메일"
             type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-              <span className={ styles.spin } ></span>
-          </div>
-
-          <div className={ styles.input }>
-              <label htmlFor="password">비밀번호</label>
-              <input
+            variant="standard"
+            color="tossmain"
+            inputProps={{ style: { fontSize: 25 } }}
+            InputLabelProps={{style: {fontSize: 22}}}
+            className={styles.inputmargin}
+        />
+        </div>
+        
+          <div>
+          <TextField
+            fullWidth
             id="password"
+            label="비밀번호"
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            variant="standard"
+            color="tossmain"
+            inputProps={{ style: { fontSize: 25 } }}
+            InputLabelProps={{style: {fontSize: 22}}}
+            className={styles.inputinput}
           />
-              <span className={ styles.spin }></span>
           </div>
+        
 
           <div className={`${styles.button} ${styles.login}`}>
-              <button type="submit"><span>로그인</span> <i class="fa fa-check"></i></button>
+              <button type="submit"><span>로그인</span></button>
           </div>
 
           <a href="" className={ styles.passforgot }>비밀번호를 잊으셨나요?</a>
 
         </div>
 
-        <div className={`${styles.materialbutton} ${styles.alt2}`}><span className={ styles.shape }></span></div>
+
+        <div className={`${styles.materialbutton} ${styles.alt2}`}><span className={ styles.shape }></span><span className={ styles.textshape }>회원가입</span></div>
+
+          
 
 
       </div>
---
+++ front/src/theme.js
@@ -0,0 +1,11 @@
+// theme.ts
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+	palette: {
+      tossmain: {
+        main: '#0064FF',
+
+      },
+    },
+});
