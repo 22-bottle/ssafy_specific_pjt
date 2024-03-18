1f9cea9e715ac06316f480af4bf74bce0080c713
CHAEEUN LEE <cheeun0903@gmail.com>
2024-03-18 11:17:16 +0900

    design: 회원가입 필요한 버튼 생성

--- front/src/components/signup/Signup.tsx
+++ front/src/components/signup/Signup.tsx
@@ -4,9 +4,18 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateField } from '@mui/x-date-pickers/DateField';
+import dayjs from 'dayjs';
 
 
-const Signup:React.FC= () => {
+function Signup() {
 
   const navigate = useNavigate();
 
@@ -19,19 +28,138 @@ const Signup:React.FC= () => {
     border: "1px solid black",       
     margin: "10px"
   };
+
+  // 성별 로직
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
     
   return (
-    <FormControl>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-        defaultValue="female"
-      >
-        <FormControlLabel value="female" control={<Radio />} label="아이" />
-        <FormControlLabel value="male" control={<Radio />} label="엄마" />
-      </RadioGroup>
-    </FormControl>
+    <div>
+      <div>
+        <div>회원가입</div>
+
+        {/* 부모 or 아이 */}
+        <div>
+          <FormControl>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+              defaultValue="female"
+            >
+              <FormControlLabel value="female" control={<Radio />} label="아이" />
+              <FormControlLabel value="male" control={<Radio />} label="엄마" />
+            </RadioGroup>
+          </FormControl>
+        </div>
+
+        {/* 성별 */}
+        <div>
+          <FormControl variant="standard" sx={{ my: 1, minWidth: 120 }}>
+            <InputLabel id="gender" sx={{fontSize: 20}}>성별</InputLabel>
+            <Select
+              labelId="gender"
+              id="gender"
+              value={age}
+              color="primary"
+              onChange={handleChange}
+              sx={{fontSize: 25}}
+            >
+              <MenuItem value="">
+              </MenuItem>
+              <MenuItem value={10} sx={{fontSize: 20}}>여자</MenuItem>
+              <MenuItem value={20} sx={{fontSize: 20}}>남자</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        {/* 생년월일 */}
+        <div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={['DateField']}>
+              <DateField
+                format="YYYY/MM/DD"
+                variant="standard"
+                color="primary"
+                label="생년월일"
+                inputProps={{ style: { fontSize: 25 } }}
+                InputLabelProps={{ style: { fontSize: 22 } }}
+              />
+            </DemoContainer>
+          </LocalizationProvider>
+        </div>
+
+        {/* 이름 */}
+        <div>
+          <TextField
+            fullWidth
+            id="name"
+            label="이름"
+            type="text"
+            variant="standard"
+            color="primary"
+            inputProps={{ style: { fontSize: 25 } }}
+            InputLabelProps={{ style: { fontSize: 22 } }}
+          />
+        </div>
+
+        {/* 이메일 */}
+        <div>
+          <TextField
+            fullWidth
+            id="email"
+            label="이메일"
+            type="text"
+            variant="standard"
+            color="primary"
+            inputProps={{ style: { fontSize: 25 } }}
+            InputLabelProps={{ style: { fontSize: 22 } }}
+          />
+        </div>
+
+        {/* 비밀번호 */}
+        <div>
+          <TextField
+            fullWidth
+            id="password"
+            label="비밀번호"
+            type="password"
+            variant="standard"
+            // Assuming tossmain is defined in your theme
+            color="primary" // Adjust based on your theme
+            inputProps={{ style: { fontSize: 25 } }}
+            InputLabelProps={{ style: { fontSize: 22 } }}
+          />
+        </div>
+
+        {/* 비밀번호 */}
+        <div>
+          <TextField
+            fullWidth
+            id="password"
+            label="비밀번호"
+            type="password"
+            variant="standard"
+            // Assuming tossmain is defined in your theme
+            color="primary" // Adjust based on your theme
+            inputProps={{ style: { fontSize: 25 } }}
+            InputLabelProps={{ style: { fontSize: 22 } }}
+          />
+        </div>
+
+        {/* 회원가입 완료 버튼 */}
+        <div>
+          <button type="submit">
+            <span>로그인</span>
+          </button>
+        </div>
+      </div>
+    </div>
+
+
     // <div style={ divBorder }>
     //   <div style={ divBorder }>
     //     <h1>회원가입<button>닫기</button></h1>
