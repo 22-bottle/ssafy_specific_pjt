56a8eeaba4e015bc70d0c39c1181bc515a77ef5c
MinjiLEE <alswl9703@naver.com>
2024-03-18 14:39:50 +0900

    design: 세계지도 나라별 지도 디자인

--- front/src/components/mainchild/Japan.tsx
+++ front/src/components/mainchild/Japan.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import styles from './Japan.module.css'
 
 const Japan:React.FC= () => {
     
     return (
         <div>
-        this is japan page
+        <div className={styles.backgroundIMG}></div>
+        <div className={styles.japanmap}></div>
+        <div className={styles.stage1}></div>
+        {/* stage가 개방되었는지 확인하고 css 다르게 적용 기능 추가해야함 */}
+        <div className={styles.unactive_stage2}></div>
+        <div className={styles.unactive_stage3}></div>
+        <div className={styles.unactive_stage4}></div>
+        <div className={styles.unactive_stage5}></div>
+
+        <div className={styles.fairy}></div>
     </div>
     );
 };
