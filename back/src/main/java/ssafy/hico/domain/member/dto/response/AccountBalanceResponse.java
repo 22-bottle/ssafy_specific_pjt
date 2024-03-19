6ebb06527b52bc0f51bb0dbde07457e67b62079c
손주현 <juhyeon042@naver.com>
2024-03-19 10:47:40 +0900

    feat: lastTransactionDate 필드 추가

--- back/src/main/java/ssafy/hico/domain/member/dto/response/AccountBalanceResponse.java
+++ back/src/main/java/ssafy/hico/domain/member/dto/response/AccountBalanceResponse.java
@@ -1,5 +1,6 @@
 package ssafy.hico.domain.member.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -32,5 +33,8 @@ public class AccountBalanceResponse {
 
         @JsonProperty("accountBalance")
         private String accountBalance;
+
+        @JsonIgnore
+        private String lastTransactionDate;
     }
 }
