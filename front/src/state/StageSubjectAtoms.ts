8afbf4018450bfd48636e5596bc5067aa5ff09c6
JIEUN LEE <jin3409975@gmail.com>
2024-03-21 09:19:35 +0900

    feat: Recoil 테스트

--- front/src/state/StageSubjectAtoms.ts
+++ front/src/state/StageSubjectAtoms.ts
@@ -1,9 +1,8 @@
 import { atom } from "recoil";
 
 export const stageSubjectState = atom({
-    // 해당 나라의 어떤 스테이지를 클릭했는지 상태 변수
-    // 각 나라별 5개의 스테이지
-    key: "stageSubjectState",
-    default: 0,
-  });
-  
+  // 해당 나라의 어떤 스테이지를 클릭했는지 상태 변수
+  // 각 나라별 5개의 스테이지
+  key: "stageSubjectState",
+  default: 0,
+});
