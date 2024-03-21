f44ed91994b8275fdf6f53188ba4028843e9e992
CHAEEUN LEE <cheeun0903@gmail.com>
2024-03-21 10:41:49 +0900

    design: 자녀추가등록 모달 디자인 변경

--- front/src/components/mainparent/ChildStatus.tsx
+++ front/src/components/mainparent/ChildStatus.tsx
@@ -154,11 +154,10 @@ const Childstatus: React.FC = () => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 500,
+    width: 480,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
     boxShadow: 40,
-    p: 4,
+    borderRadius: '8px',
   }
 
   return (
