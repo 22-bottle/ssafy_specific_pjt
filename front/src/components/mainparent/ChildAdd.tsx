220f71864909c654cda4d0e1675f216d84d66094
CHAEEUN LEE <cheeun0903@gmail.com>
2024-03-21 10:42:18 +0900

    chore: 주석 삭제

--- front/src/components/mainparent/ChildAdd.tsx
+++ front/src/components/mainparent/ChildAdd.tsx
@@ -81,33 +81,6 @@ const Childadd: React.FC = () => {
         </div>
       </div>
     </div>
-    // <div className="no-child-container" style={styleObj}>
-    //   <div className="header" style={styleObj}>
-    //     <div className="logo">toss</div>
-    //     {/* Other header elements */}
-    //   </div>
-
-    //   <div className="no-child-content" style={styleObj}>
-    //     <h2>아이 등록 코드</h2>
-    //     <div className="no-child-illustration" style={styleObj}>
-    //       {/* Illustration or image */}
-    //       {/* <img src="/path-to-your-illustration.png" alt="No Child image" /> */}
-    //     </div>
-    //     <div className="no-child-info" style={styleObj}>
-    //       <p>아이 등록을 위한 코드를 만들었어요</p>
-    //       <p>아이 계정에서 코드를 입력하세요</p>
-    //     </div>
-    //     <div className="registration-code" style={styleObj}>
-    //       <input type="text" value={registCode} readOnly />
-    //     </div>
-    //     <div className="action-buttons">
-    //       <button onClick={handleCopyCode}>내 코드 복사하기</button>
-    //       <button>카카오톡 공유하기</button>
-    //     </div>
-    //   </div>
-
-    //   <div className="footer">{/* Footer content */}</div>
-    // </div>
   )
 }
 
