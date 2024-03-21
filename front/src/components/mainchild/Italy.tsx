1bafb0d3de6c32e5b565efe076f861082ec4c234
JIEUN LEE <jin3409975@gmail.com>
2024-03-21 14:47:37 +0900

    feat: 스테이지 선택화면 설명창

--- front/src/components/mainchild/Italy.tsx
+++ front/src/components/mainchild/Italy.tsx
@@ -1,26 +1,41 @@
-import React, { useState, startTransition } from "react";
-import { useNavigate } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { stageSubjectState } from "@/state/StageSubjectAtoms";
+import React, { startTransition, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
+import { stageSubjectState } from '@/state/StageSubjectAtoms'
 
-import styles from "./Italy.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestion, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+import styles from './Italy.module.css'
 
 const Italy: React.FC = () => {
-  const [stageId, setStageId] = useRecoilState(stageSubjectState);
+  const [stageId, setStageId] = useRecoilState(stageSubjectState)
 
-  const navigate = useNavigate();
+  // 물음표 아이콘 클릭
+  const [showDescription, setShowDescription] = useState(false)
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription)
+  }
+
+  const closeDescription = () => {
+    setShowDescription(false)
+  }
+
+  const navigate = useNavigate()
 
   const stageStart = (Id: number) => {
     // 상태 업데이트 함수를 사용하여 stageId 상태를 변경
-    setStageId(Id);
+    setStageId(Id)
     // startTransition을 사용하여 비동기 업데이트 처리
     startTransition(() => {
-      navigate("/mainchild/stage/Italy");
-    });
-  };
+      navigate('/mainchild/stage/Italy')
+    })
+  }
 
   return (
     <div>
+      <h1 className={styles.title}>Stage를 클릭해서 퀴즈를 시작하세요!</h1>
       <div className={styles.backgroundIMG}></div>
       <div className={styles.italymap}></div>
       <div onClick={() => stageStart(0)} className={styles.stage1}></div>
@@ -42,9 +57,31 @@ const Italy: React.FC = () => {
         className={styles.unactive_stage5}
       ></div>
 
-      <div className={styles.fairy}></div>
+      <div className={styles.fairyContainer}>
+        <div className={styles.fairy}>
+          {!showDescription && (
+            <FontAwesomeIcon
+              icon={faQuestion}
+              className={styles.questionIcon}
+              onClick={toggleDescription}
+            />
+          )}
+        </div>
+        {showDescription && (
+          <div className={styles.description}>
+            <button className={styles.closeButton} onClick={closeDescription}>
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+            <p>
+              뭐? 연료를 달라고? 로마에선 로마법을 따라야지!
+              <br />
+              로마에 공짜는 없어! 문제를 맞춘다면 생각해볼게.
+            </p>
+          </div>
+        )}
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Italy;
+export default Italy
