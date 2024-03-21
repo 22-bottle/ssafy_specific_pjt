048ea50fa08161cf717e118cbb261e916b7014c9
CHAEEUN LEE <cheeun0903@gmail.com>
2024-03-21 09:15:42 +0900

    feat: result

--- front/src/components/mainchild/Result.tsx
+++ front/src/components/mainchild/Result.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React from 'react'
 import styles from '@/components/mainchild/Result.module.css'
-const  quizsuccess = require('@/assets/success.png');
-const quizfailed = require('@/assets/failed.png');
+const quizsuccess = require('@/assets/success.png')
+const quizfailed = require('@/assets/failed.png')
 
-const Result:React.FC= () => {
-    
-    return (
-        <div className={styles.componentContainer}>
+const Result: React.FC = () => {
+  return (
+    <div className={styles.componentContainer}>
       <div className={styles.header}>
-        스테이지 3
-        <button className={styles.closeButton}>X</button>
+        스테이지 3<button className={styles.closeButton}>X</button>
       </div>
       <div className={styles.title}>10개 중 9개 정답</div>
       {/* if문으로  성공 실패 렌더링 다르게*/}
-      <img src={quizsuccess} alt="Character" className={styles.characterImage} />
+      <img
+        src={quizsuccess}
+        alt="Character"
+        className={styles.characterImage}
+      />
       <div className={styles.info}>
         <div>성공</div>
         <div>연료 5% 충전</div>
@@ -21,7 +23,7 @@ const Result:React.FC= () => {
       </div>
       <button className={styles.button}>다음 스테이지</button>
     </div>
-    );
-};
+  )
+}
 
-export default Result;
+export default Result
