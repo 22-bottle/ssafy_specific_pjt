1de803138704a5099c76056a7f70808be0a54edf
CHAEEUN LEE <cheeun0903@gmail.com>
2024-03-22 15:04:18 +0900

    chore: 주석 삭제

--- front/src/components/parentwallet/SendingComplete.tsx
+++ front/src/components/parentwallet/SendingComplete.tsx
@@ -1,19 +1,9 @@
 import React from 'react'
-// import { useNavigate } from 'react-router-dom'
 import styles from './sendingcomplete.module.css'
 import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded'
 import Button from '@mui/material/Button'
 
 const Sendingcomplete: React.FC = () => {
-  // const styleObj = {
-  //   border: '1px solid black',
-  //   padding: '10px',
-  //   margin: '10px',
-  // }
-  // const navigate = useNavigate()
-  // const handleButtonClick = () => {
-  //   navigate('/parentwallet')
-  // }
   return (
     <div className={styles.container}>
       {/* 메인 */}
@@ -46,32 +36,6 @@ const Sendingcomplete: React.FC = () => {
         </div>
       </div>
     </div>
-    // <div className="sending-complete-container" style={styleObj}>
-    //   <div className="sending-complete-card" style={styleObj}>
-    //     <div className="sending-complete-header" style={styleObj}>
-    //       {/* Toss logo */}
-    //       <img src="/path/to/logo.png" alt="Toss Logo" className="toss-logo" />
-    //     </div>
-    //     <div className="sending-complete-content" style={styleObj}>
-    //       <div className="checkmark-icon" style={styleObj}>
-    //         {/* 송금 완료 체크 아이콘 */}
-    //       </div>
-    //       <div className="confirmation-message" style={styleObj}>
-    //         <p>이승재님에게</p>
-    //         <br />
-    //         <p>3,000원을 보냈습니다!</p>
-    //       </div>
-    //       <div className="sub-message" style={styleObj}>
-    //         수수료는 토스가 냈어요!
-    //       </div>
-    //     </div>
-    //     <div className="sending-complete-footer" style={styleObj}>
-    //       <button className="confirm-button" onClick={handleButtonClick}>
-    //         확인
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
