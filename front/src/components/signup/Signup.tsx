81135d10f617e0171433f23f83e075f9e865bb01
MinjiLEE <alswl9703@naver.com>
2024-03-22 13:41:59 +0900

    feat: signup 변수 입력

--- front/src/components/signup/Signup.tsx
+++ front/src/components/signup/Signup.tsx
@@ -1,53 +1,66 @@
-import React, { useState } from "react";
-import styles from './signup.module.css';
-import { useNavigate } from 'react-router-dom'; 
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import TextField from '@mui/material/TextField';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateField } from '@mui/x-date-pickers/DateField';
-import { Typography } from '@mui/material';
-import { Grid } from '@mui/material';
-import { IconButton } from '@mui/material';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-
-
+import React, { useState, startTransition } from 'react'
+import styles from './signup.module.css'
+import { useNavigate } from 'react-router-dom'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import TextField from '@mui/material/TextField'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DateField } from '@mui/x-date-pickers/DateField'
+import { Typography } from '@mui/material'
+import { Grid } from '@mui/material'
+import { IconButton } from '@mui/material'
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
+import dayjs from 'dayjs'
 
 function Signup() {
-
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   // 계좌 등록 화면으로 이동해야 한다. 경로 변경 필요!
   const completeClick = () => {
-    navigate('/mainparent/childstatus');
-  };
+    //날짜형식 변경
+    const birthDateformated = birthDate.format('YYYY-MM-DD')
+    console.log(
+      'signup 입력 변수 확인',
+      role,
+      gender,
+      name,
+      email,
+      password,
+      confirm,
+      code,
+      birthDateformated
+    )
 
-  const divBorder = {
-    border: "1px solid black",       
-    margin: "10px"
-  };
+    // api 함수에 보내기
+    startTransition(() => {
+      navigate('/mainparent/childstatus')
+    })
+  }
+  // 이메일
+  const [email, setEmail] = useState('')
+  // 비밀번호
+  const [password, setPassword] = useState('')
+  //비밀번호 확인
+  const [confirm, setConfirm] = useState('')
+  //이름
+  const [name, setName] = useState('')
+  //생년월일
+  const [birthDate, setBirthDate] = useState(dayjs('1111-11-11'))
 
-// 초대코드 로직
-const [selectedValue, setSelectedValue] = useState('female');
+  // 성별 판별
+  const [gender, setGender] = useState('female')
+  // 부모, 아이 역할
+  const [role, setRole] = useState('PARENT')
+  // 초대 코드
+  const [code, setCode] = useState('')
 
-const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  setSelectedValue(event.target.value);
-};
-
-// 성별 로직
-const [age, setAge] = useState('');
-
-const handleSelectChange = (event: SelectChangeEvent) => {
-  setAge(event.target.value);
-};
-    
   return (
     <div className={styles.materialContainer}>
       <div className={styles.thirdbox}></div>
@@ -57,7 +70,7 @@ const handleSelectChange = (event: SelectChangeEvent) => {
           <div className={styles.title}>회원가입</div>
           {/* 닫기 버튼 */}
           <IconButton aria-label="close" size="large" className={styles.close}>
-              <CloseRoundedIcon style={{ color: 'white', fontSize: 40 }} />
+            <CloseRoundedIcon style={{ color: 'white', fontSize: 40 }} />
           </IconButton>
         </div>
 
@@ -69,42 +82,63 @@ const handleSelectChange = (event: SelectChangeEvent) => {
                 row
                 aria-labelledby="demo-row-radio-buttons-group-label"
                 name="row-radio-buttons-group"
-                defaultValue="female"
-                value={selectedValue}
-                onChange={handleRadioChange}
+                defaultValue="ROLE"
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
               >
                 <FormControlLabel
-                  value="female"
-                  control={<Radio sx={{
-                    color: 'white',
-                    '&.Mui-checked': {
-                      color: 'white',
-                    },
-                    '& .MuiSvgIcon-root': {
-                      fontSize: 25,
-                    },
-                  }} />}
-                  label={<Typography sx={{ fontSize: 18, color: 'white' }}>아이</Typography>}
+                  value="CHILD"
+                  control={
+                    <Radio
+                      sx={{
+                        color: 'white',
+                        '&.Mui-checked': {
+                          color: 'white',
+                        },
+                        '& .MuiSvgIcon-root': {
+                          fontSize: 25,
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <Typography sx={{ fontSize: 18, color: 'white' }}>
+                      아이
+                    </Typography>
+                  }
                 />
                 <FormControlLabel
-                  value="male"
-                  control={<Radio sx={{
-                    color: 'white',
-                    '&.Mui-checked': {
-                      color: 'white',
-                    },
-                    '& .MuiSvgIcon-root': {
-                      fontSize: 25,
-                    },
-                  }} />}
-                  label={<Typography sx={{ fontSize: 18, color: 'white' }}>부모</Typography>}
+                  value="PARENT"
+                  control={
+                    <Radio
+                      sx={{
+                        color: 'white',
+                        '&.Mui-checked': {
+                          color: 'white',
+                        },
+                        '& .MuiSvgIcon-root': {
+                          fontSize: 25,
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <Typography sx={{ fontSize: 18, color: 'white' }}>
+                      부모
+                    </Typography>
+                  }
                 />
               </RadioGroup>
             </FormControl>
           </Grid>
           {/* 부모님 초대코드, 아이를 선택했을 때만 표시 */}
-          {selectedValue === 'female' && (
-            <Grid item xs={12} sm={6} className={`${styles.gap5} ${styles.paddings}`}>
+          {role === 'CHILD' && (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              className={`${styles.gap5} ${styles.paddings}`}
+            >
               <TextField
                 fullWidth
                 id="parentCode"
@@ -112,18 +146,20 @@ const handleSelectChange = (event: SelectChangeEvent) => {
                 type="text"
                 variant="standard"
                 color="primary"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
                 inputProps={{
                   style: {
                     fontSize: 22,
                     color: 'white',
-                    caretColor: 'white'
-                  }
+                    caretColor: 'white',
+                  },
                 }}
                 InputLabelProps={{
                   style: {
                     fontSize: 20,
                     color: 'white',
-                  }
+                  },
                 }}
                 sx={{
                   '& .MuiInput-underline:before': {
@@ -144,83 +180,102 @@ const handleSelectChange = (event: SelectChangeEvent) => {
             </Grid>
           )}
         </Grid>
-        
+
         <Grid container spacing={3} className={styles.gap1}>
           {/* 성별 */}
           <Grid item xs={12} sm={4} className={styles.paddings}>
-            <FormControl variant="standard" sx={{ my: 1, minWidth: 120, color: 'white' }}>
-            <InputLabel id="gender" sx={{ color: 'white', fontSize: 20, '&.Mui-focused': { color: 'white' } }}>성별</InputLabel>            
-            <Select
-              labelId="gender"
-              id="gender"
-              value={age}
-              onChange={handleSelectChange}
-              sx={{
-                color: 'white',
-                fontSize: 22,
-                '&:before': {
-                  borderBottom: '2px solid #B0BEC5',
-                },
-                '&:hover:before': {
-                  borderBottom: '2px solid white !important',
-                },
-                
-                '&:after': {
-                  borderBottomColor: "white",
-                },
-                '& .MuiSvgIcon-root': {
-                  color: "white",
-                },
-              }}
+            <FormControl
+              variant="standard"
+              sx={{ my: 1, minWidth: 120, color: 'white' }}
             >
-                <MenuItem value="">
+              <InputLabel
+                id="gender"
+                sx={{
+                  color: 'white',
+                  fontSize: 20,
+                  '&.Mui-focused': { color: 'white' },
+                }}
+              >
+                성별
+              </InputLabel>
+              <Select
+                labelId="gender"
+                id="gender"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+                sx={{
+                  color: 'white',
+                  fontSize: 22,
+                  '&:before': {
+                    borderBottom: '2px solid #B0BEC5',
+                  },
+                  '&:hover:before': {
+                    borderBottom: '2px solid white !important',
+                  },
+
+                  '&:after': {
+                    borderBottomColor: 'white',
+                  },
+                  '& .MuiSvgIcon-root': {
+                    color: 'white',
+                  },
+                }}
+              >
+                <MenuItem value=""></MenuItem>
+                <MenuItem value={10} sx={{ fontSize: 20 }}>
+                  여자
                 </MenuItem>
-                <MenuItem value={10} sx={{fontSize: 20}}>여자</MenuItem>
-                <MenuItem value={20} sx={{fontSize: 20}}>남자</MenuItem>
+                <MenuItem value={20} sx={{ fontSize: 20 }}>
+                  남자
+                </MenuItem>
               </Select>
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={8} className={styles.paddings}>
             {/* 생년월일 */}
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <div style={{ width: '215px', marginLeft: 25 }}>
-              <DemoContainer components={['DateField']}>
-                <DateField
-                  format="YYYY/MM/DD"
-                  variant="standard"
-                  color="primary"
-                  label="생년월일"
-                  inputProps={{
-                    style: {
-                      fontSize: 22,
-                      color: 'white',
-                      caretColor: 'white' 
-                    }
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      fontSize: 20,
-                      color: 'white', 
-                    }
-                  }}
-                  sx={{
-                    '& .MuiInput-underline:before': { 
-                      borderBottom: '2px solid #B0BEC5',
-                    },
-                    '& .MuiInput-underline:after': { 
-                      borderBottomColor: 'white',
-                    },
-                    '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
-                      borderBottomColor: 'white',
-                    },
-                    '& .MuiInputBase-input::placeholder': { 
-                      color: 'white',
-                      opacity: 1,
-                    },
-                  }}
-                />
-              </DemoContainer>
-            </div>
+              <div style={{ width: '215px', marginLeft: 25 }}>
+                <DemoContainer components={['DateField']}>
+                  <DateField
+                    format="YYYY/MM/DD"
+                    variant="standard"
+                    color="primary"
+                    label="생년월일"
+                    value={birthDate}
+                    onChange={(newValue) => {
+                      setBirthDate(newValue ? newValue : dayjs('1111-11-11'))
+                    }}
+                    inputProps={{
+                      style: {
+                        fontSize: 22,
+                        color: 'white',
+                        caretColor: 'white',
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        fontSize: 20,
+                        color: 'white',
+                      },
+                    }}
+                    sx={{
+                      '& .MuiInput-underline:before': {
+                        borderBottom: '2px solid #B0BEC5',
+                      },
+                      '& .MuiInput-underline:after': {
+                        borderBottomColor: 'white',
+                      },
+                      '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+                        borderBottomColor: 'white',
+                      },
+                      '& .MuiInputBase-input::placeholder': {
+                        color: 'white',
+                        opacity: 1,
+                      },
+                    }}
+                  />
+                </DemoContainer>
+              </div>
             </LocalizationProvider>
           </Grid>
         </Grid>
@@ -235,30 +290,32 @@ const handleSelectChange = (event: SelectChangeEvent) => {
               type="text"
               variant="standard"
               color="primary"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               inputProps={{
                 style: {
                   fontSize: 22,
                   color: 'white',
-                  caretColor: 'white' 
-                }
+                  caretColor: 'white',
+                },
               }}
               InputLabelProps={{
                 style: {
                   fontSize: 20,
-                  color: 'white', 
-                }
+                  color: 'white',
+                },
               }}
               sx={{
-                '& .MuiInput-underline:before': { 
+                '& .MuiInput-underline:before': {
                   borderBottom: '2px solid #B0BEC5',
                 },
-                '& .MuiInput-underline:after': { 
+                '& .MuiInput-underline:after': {
                   borderBottomColor: 'white',
                 },
                 '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
                   borderBottomColor: 'white',
                 },
-                '& .MuiInputBase-input::placeholder': { 
+                '& .MuiInputBase-input::placeholder': {
                   color: 'white',
                   opacity: 1,
                 },
@@ -274,30 +331,32 @@ const handleSelectChange = (event: SelectChangeEvent) => {
               type="text"
               variant="standard"
               color="primary"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               inputProps={{
                 style: {
                   fontSize: 22,
                   color: 'white',
-                  caretColor: 'white' 
-                }
+                  caretColor: 'white',
+                },
               }}
               InputLabelProps={{
                 style: {
                   fontSize: 20,
-                  color: 'white', 
-                }
+                  color: 'white',
+                },
               }}
               sx={{
-                '& .MuiInput-underline:before': { 
+                '& .MuiInput-underline:before': {
                   borderBottom: '2px solid #B0BEC5',
                 },
-                '& .MuiInput-underline:after': { 
+                '& .MuiInput-underline:after': {
                   borderBottomColor: 'white',
                 },
                 '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
                   borderBottomColor: 'white',
                 },
-                '& .MuiInputBase-input::placeholder': { 
+                '& .MuiInputBase-input::placeholder': {
                   color: 'white',
                   opacity: 1,
                 },
@@ -316,18 +375,20 @@ const handleSelectChange = (event: SelectChangeEvent) => {
               type="password"
               variant="standard"
               color="primary"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               inputProps={{
                 style: {
                   fontSize: 22,
                   color: 'white',
-                  caretColor: 'white'
-                }
+                  caretColor: 'white',
+                },
               }}
               InputLabelProps={{
                 style: {
                   fontSize: 20,
                   color: 'white',
-                }
+                },
               }}
               sx={{
                 '& .MuiInput-underline:before': {
@@ -346,7 +407,7 @@ const handleSelectChange = (event: SelectChangeEvent) => {
               }}
             />
           </Grid>
-          
+
           {/* 비밀번호 확인 필드 */}
           <Grid item xs={12} sm={6} className={styles.paddings}>
             <TextField
@@ -356,18 +417,20 @@ const handleSelectChange = (event: SelectChangeEvent) => {
               type="password"
               variant="standard"
               color="primary"
+              value={confirm}
+              onChange={(e) => setConfirm(e.target.value)}
               inputProps={{
                 style: {
                   fontSize: 22,
                   color: 'white',
-                  caretColor: 'white'
-                }
+                  caretColor: 'white',
+                },
               }}
               InputLabelProps={{
                 style: {
                   fontSize: 20,
                   color: 'white',
-                }
+                },
               }}
               sx={{
                 '& .MuiInput-underline:before': {
@@ -390,13 +453,13 @@ const handleSelectChange = (event: SelectChangeEvent) => {
 
         {/* 회원가입 완료 버튼 */}
         <div className={`${styles.button} ${styles.login}`}>
-          <button type="submit">
+          <button type="submit" onClick={completeClick}>
             <span>회원가입</span>
           </button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Signup;
+export default Signup
