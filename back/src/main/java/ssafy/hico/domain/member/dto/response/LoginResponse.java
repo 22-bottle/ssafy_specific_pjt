18df6bf98d4e7f1e0762d3234780b9559db19487
손주현 <juhyeon042@naver.com>
2024-03-25 17:26:45 +0900

    feat: LoginResponse 역할 추가

--- back/src/main/java/ssafy/hico/domain/member/dto/response/LoginResponse.java
+++ back/src/main/java/ssafy/hico/domain/member/dto/response/LoginResponse.java
@@ -3,6 +3,7 @@ package ssafy.hico.domain.member.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import ssafy.hico.domain.member.entity.Role;
 import ssafy.hico.global.jwt.TokenResponse;
 
 @Data
@@ -12,4 +13,5 @@ public class LoginResponse {
     private TokenResponse tokenResponse;
     private boolean isAccount;
     private String name;
+    private Role role;
 }
