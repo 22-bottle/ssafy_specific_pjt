a4678673cc306a6f15379881a79c3b0e1174dfcb
MinjiLEE <alswl9703@naver.com>
2024-03-25 16:05:37 +0900

    feat: 보유자녀에 따른 페이지 렌더링

--- front/src/components/mainparent/MainParent.tsx
+++ front/src/components/mainparent/MainParent.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import ChildStatus from './ChildStatus'
-const Mainparent:React.FC= () => {
-    
-    return (
-        <div>
-            <ChildStatus/>
-        </div>
-        
-        
-    );
-};
+import NoChild from './NoChild'
+import { getChild } from '@/api/parent'
 
-export default Mainparent;
+const MainParent: React.FC = () => {
+  // childrenlist 상태 초기화
+  const [childrenList, setChildrenList] = useState([])
+
+  useEffect(() => {
+    // 비동기 함수 정의
+    const fetchChildren = async () => {
+      try {
+        const response = await getChild()
+        const childrenData = response.data.data
+        console.log(childrenData)
+        // API 응답으로 받은 데이터를 상태에 저장
+        setChildrenList(childrenData)
+      } catch (error) {
+        console.error('API 요청 중 오류 발생:', error)
+      }
+    }
+
+    // 비동기 함수 실행
+    fetchChildren()
+  }, []) // 빈 배열을 넣어서 컴포넌트 마운트 시에만 실행되도록 함
+
+  return <div>{childrenList.length === 0 ? <NoChild /> : <ChildStatus />}</div>
+}
+
+export default MainParent
