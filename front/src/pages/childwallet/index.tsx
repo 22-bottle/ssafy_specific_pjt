b99d1ad2b3bf2ec8a2e1731eacb3cc0caa0f4292
Chaeeun Lee <cheeun0903@gmail.com>
2024-03-26 04:43:41 +0900

    design: navbar 추가

--- front/src/pages/childwallet/index.tsx
+++ front/src/pages/childwallet/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Navbar from './navbar';
 import { Outlet } from "react-router-dom";
 
 const ChildWallet: React.FC = () => {
   return (
     <div>
+      <Navbar />
       {/* this is ChildWallet */}
-      <Outlet />
+      <div style={{
+        paddingTop: '70px',
+        backgroundColor: "#F9FAFB",
+        position: "relative",
+        minHeight: "calc(100vh - 70px)",
+      }}>
+        <Outlet />
+      </div>
     </div>
   );
 };
