0c4d6870df9e86ce66360a9db804db2a717c4686
손주현 <juhyeon042@naver.com>
2024-03-28 16:00:00 +0900

    feat: config 파일 수정

--- back/src/main/java/ssafy/hico/config/WebConfig.java
+++ back/src/main/java/ssafy/hico/config/WebConfig.java
@@ -13,7 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
