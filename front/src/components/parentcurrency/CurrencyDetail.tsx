1b5a93c03e9ea06d0a5274433895b12895bba4e9
MinjiLEE <alswl9703@naver.com>
2024-03-28 17:24:26 +0900

    design: currency-detail

--- front/src/components/parentcurrency/CurrencyDetail.tsx
+++ front/src/components/parentcurrency/CurrencyDetail.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ApexCharts from 'apexcharts'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { currencydataList } from '@/state/currencyselectors'
 import styles from './CurrencyDetail.module.css'
 import { Height } from '@mui/icons-material'
 import { currencydetailState } from '@/state/currencyatoms'
+import { today } from '@/api/currency'
+import updown from '../../assets/updow.png'
 const CurrencyDetail: React.FC = () => {
   const currencyData = useRecoilValue(currencydataList) || []
   const chartRef = useRef<HTMLDivElement | null>(null)
@@ -103,9 +105,43 @@ const CurrencyDetail: React.FC = () => {
 
   const [contryId, setCountryId] = useRecoilState(currencydetailState)
   const countries = ['', '미국 USD', '일본 JPY', '유럽 EUR', '중국 CNY']
+  const [usaBasicRate, setUsaBasicRate] = useState('')
+  const [euBasicRate, setEuBasicRate] = useState('')
+  const [japanBasicRate, setJapanBasicRate] = useState('')
+  const [chinaBasicRate, setChinaBasicRate] = useState('')
+  const [usaAmount, setUsaAmount] = useState('')
+  const [euAmount, setEuAmount] = useState('')
+  const [japanAmount, setJapanAmount] = useState('')
+  const [chinaAmount, setChinaAmount] = useState('')
+  useEffect(() => {
+    const fetchTodayCurrency = async () => {
+      try {
+        const response = await today()
+        setUsaBasicRate(response.data.data[0].basicRate)
+        setJapanBasicRate(response.data.data[1].basicRate)
+        setEuBasicRate(response.data.data[2].basicRate)
+        setChinaBasicRate(response.data.data[3].basicRate)
+        setUsaAmount(response.data.data[0].amount)
+        setJapanAmount(response.data.data[1].amount)
+        setEuAmount(response.data.data[2].amount)
+        setChinaAmount(response.data.data[3].amount)
+      } catch (error) {
+        console.error('API 요청 중 오류 발생: ', error)
+      }
+    }
+    fetchTodayCurrency()
+  }, [])
   return (
     <div className={styles.container}>
       <div className={styles.header}>{countries[contryId]}</div>
+      <div>
+        실시간 환율 {usaBasicRate}원 어제보다 {usaAmount}원{' '}
+        <img
+          src={updown}
+          alt="boy"
+          style={{ height: '22px', marginTop: 3, marginLeft: '15px' }}
+        />
+      </div>
       <div className={styles.chart}>
         <div ref={chartRef} />
       </div>
