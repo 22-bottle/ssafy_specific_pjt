9e6e41f92eeea50970e956a83893be87a717ee13
손주현 <juhyeon042@naver.com>
2024-03-28 12:45:40 +0900

    feat: 로그아웃 구현

--- back/src/main/java/ssafy/hico/domain/member/controller/MemberController.java
+++ back/src/main/java/ssafy/hico/domain/member/controller/MemberController.java
@@ -4,10 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 import ssafy.hico.domain.member.dto.request.MemberLoginRequest;
 import ssafy.hico.domain.member.dto.request.MemberSignUpRequest;
 import ssafy.hico.domain.member.service.MemberService;
+import ssafy.hico.global.annotation.LoginOnly;
 import ssafy.hico.global.response.success.SuccessCode;
 
 import static ssafy.hico.global.response.success.CommonResponseEntity.getResponseEntity;
@@ -34,6 +36,14 @@ public class MemberController {
     @PostMapping("/token")
     public ResponseEntity<?> recreateToken(@RequestHeader(value = "Authorization", required = false) String bearerToken){
         return getResponseEntity(SuccessCode.OK, memberService.recreateToken(bearerToken));
+    }
+
+    @PostMapping("/logout")
+    @LoginOnly(level = LoginOnly.Level.ALL)
+    public ResponseEntity<?> recreateToken(HttpServletRequest httpServletRequest){
+        Long memberId = (Long) httpServletRequest.getAttribute("memberId");
+        memberService.logout(memberId);
+        return getResponseEntity(SuccessCode.OK);
     }
 
 
