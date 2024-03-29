37efd9c064856c08cf802e4bcace202ed9f7ab7d
JIEUN LEE <jin3409975@gmail.com>
2024-03-29 17:12:10 +0900

    feat: API 연결 완료

--- front/src/api/account.ts
+++ front/src/api/account.ts
@@ -1,28 +1,28 @@
 import { http } from '@/axios'
 
 const api = {
-    list: '/account',
-    register : '/account',
-    make : '/account/make'
+  list: '/account',
+  register: '/account',
+  make: '/account/make',
 }
 
-async function list(){
-    return await http.get(api.list)
+async function list() {
+  return await http.get(api.list)
 }
 
-async function register(accountNo : string, password : string){
-    const requestBody = {
-        accountNo : accountNo,
-        password : password
-    }
-    return await http.post(api.register, requestBody);
+async function register(accountNo: string, password: string) {
+  const requestBody = {
+    accountNo: accountNo,
+    password: password,
+  }
+  return await http.post(api.register, requestBody)
 }
 
-async function make(password : string){
-    const requestBody = {
-        password : password
-    }
-    return await http.post(api.make, requestBody);
+async function make(password: string) {
+  const requestBody = {
+    password: password,
+  }
+  return await http.post(api.make, requestBody)
 }
 
-export {list, register, make}
+export { list, register, make }
