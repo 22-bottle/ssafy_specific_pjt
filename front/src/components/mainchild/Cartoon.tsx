a3c27ff65650cf412bc4f18b53fe7537a5a671c5
손주현 <juhyeon042@naver.com>
2024-04-01 11:09:04 +0900

    design: cartoon 디자인 수정

--- front/src/components/mainchild/Cartoon.tsx
+++ front/src/components/mainchild/Cartoon.tsx
@@ -9,6 +9,7 @@ import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
 import play from '../../assets/play.png'
 import stop from '../../assets/stop.png'
+import arrow from '../../assets/arrow.png'
 
 const Cartoon: React.FC = () => {
     const navigate = useNavigate()
@@ -18,6 +19,7 @@ const Cartoon: React.FC = () => {
     const [open, setOpen] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false); // TTS 재생 상태를 추적하는 상태
     const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
+    const [currentText, setCurrentText] = useState("TTS 재생하여 설명을 들을 수 있어요 :)");
 
     const [animation, setAnimation] = useState('');
 
@@ -60,9 +62,12 @@ const Cartoon: React.FC = () => {
 
     const speakTextPart = (index: number, textParts: string[]): void => {
         if (index >= textParts.length) {
-            setIsPlaying(false); // 모든 텍스트 부분이 말해진 후
+            setIsPlaying(false);
+            setCurrentText(""); // Clear the text when finished
             return;
         }
+
+        setCurrentText(textParts[index]); // Update the currently speaking text
 
         const utterance: SpeechSynthesisUtterance = new SpeechSynthesisUtterance(textParts[index]);
         if (voices.length > 1) {
@@ -94,7 +99,7 @@ const Cartoon: React.FC = () => {
         changePage(prevIndex);
     };
 
-// 다음 페이지로 가는 함수
+    // 다음 페이지로 가는 함수
     const goToNextCartoon = () => {
         if (isPlaying) {
             speechSynthesis.cancel();
@@ -135,19 +140,21 @@ const Cartoon: React.FC = () => {
 
     return (
         <div className={styles.mainContainer}>
-            <div className={`${styles.cartoonContainer} ${styles[animation]}`} style={{ position: 'relative' }}>
-                <img src={cartoonList[currentCartoonIndex].pageImg} alt="만화" />
-
-                {/* 재생/중지 버튼 */}
-                <button onClick={playTTS} className={styles.audioButton}>
-                    <img src={isPlaying ? stop : play} alt={isPlaying ? "중지" : "재생"} />
-                </button>
+            <div className={`${styles.cartoonContainer} ${styles[animation]}`}>
+                <img src={cartoonList[currentCartoonIndex].pageImg} alt="만화" className={styles.cartoonImage} />
             </div>
 
             {/* 이전/다음 페이지 버튼 */}
             <div className={styles.buttonContainer}>
-                <button className={styles.backButton} onClick={goToPreviousCartoon}></button>
-                <button className={styles.frontButton} onClick={goToNextCartoon}></button>
+                <button className={styles.backButton} onClick={goToPreviousCartoon}><img src={arrow}/></button>
+                <button className={styles.frontButton} onClick={goToNextCartoon}><img src={arrow} /></button>
+            </div>
+            {/* 재생/중지 버튼 */}
+            <div className={styles.ttsContainer}>
+                <button onClick={playTTS} className={styles.audioButton}>
+                    <img src={isPlaying ? stop : play} alt={isPlaying ? "중지" : "재생"} />
+                </button>
+                <div className={styles.ttsText}>{currentText}</div>
             </div>
 
             <Modal
