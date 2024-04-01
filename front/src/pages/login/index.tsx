03d7308155047cdfecff76f454973600daeb2b07
JIEUN LEE <jin3409975@gmail.com>
2024-04-01 13:14:41 +0900

    design: 로그인 & 회원가입 화면 로고 삽입

--- front/src/pages/login/index.tsx
+++ front/src/pages/login/index.tsx
@@ -4,6 +4,7 @@ import TextField from '@mui/material/TextField'
 import { login, token } from '@/api/member'
 import { useNavigate } from 'react-router-dom'
 import axios, { AxiosError } from 'axios'
+import logo from '../../assets/logo.png'
 
 interface ErrorResponse {
   statusCode: number
@@ -89,66 +90,67 @@ function Login() {
     }
   }
   return (
-    <div className={styles.materialContainer}>
-      <div className={styles.box}>
-        <div className={styles.title}>로그인</div>
-
-        {/* 이메일 */}
-        <div>
-          <TextField
-            fullWidth
-            id="username"
-            label="이메일"
-            type="text"
-            variant="standard"
-            color="primary"
-            value={useremail}
-            onChange={(e) => setUseremail(e.target.value)}
-            inputProps={{ style: { fontSize: 25 } }}
-            InputLabelProps={{ style: { fontSize: 22 } }}
-            className={styles.inputmargin}
-          />
-        </div>
-
-        {/* 비밀번호 */}
-        <div>
-          <TextField
-            fullWidth
-            id="password"
-            label="비밀번호"
-            type="password"
-            variant="standard"
-            color="primary"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            inputProps={{ style: { fontSize: 25 } }}
-            InputLabelProps={{ style: { fontSize: 22 } }}
-            className={styles.inputinput}
-          />
-        </div>
-
-        {/* 로그인 완료 버튼 */}
-        <div
-          onClick={completeClick}
-          className={`${styles.button} ${styles.login}`}
-        >
-          <button type="submit">
-            <span>로그인</span>
-          </button>
-        </div>
-
-        <a href="#" className={styles.passforgot}>
-          비밀번호를 잊으셨나요?
-        </a>
+    <div>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="로고" />
       </div>
+      <div className={styles.materialContainer}>
+        <div className={styles.box}>
+          <div className={styles.title}>로그인</div>
 
-      {/* 회원가입 버튼 */}
-      <div
-        onClick={signupClick}
-        className={`${styles.materialbutton} ${styles.alt2}`}
-      >
-        <span className={styles.shape}></span>
-        <span className={styles.textshape}>회원가입</span>
+          {/* 이메일 */}
+          <div>
+            <TextField
+              fullWidth
+              id="username"
+              label="이메일"
+              type="text"
+              variant="standard"
+              color="primary"
+              value={useremail}
+              onChange={(e) => setUseremail(e.target.value)}
+              inputProps={{ style: { fontSize: 25 } }}
+              InputLabelProps={{ style: { fontSize: 22 } }}
+              className={styles.inputmargin}
+            />
+          </div>
+
+          {/* 비밀번호 */}
+          <div>
+            <TextField
+              fullWidth
+              id="password"
+              label="비밀번호"
+              type="password"
+              variant="standard"
+              color="primary"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              inputProps={{ style: { fontSize: 25 } }}
+              InputLabelProps={{ style: { fontSize: 22 } }}
+              className={styles.inputinput}
+            />
+          </div>
+
+          {/* 로그인 완료 버튼 */}
+          <div
+            onClick={completeClick}
+            className={`${styles.button} ${styles.login}`}
+          >
+            <button type="submit">
+              <span>로그인</span>
+            </button>
+          </div>
+        </div>
+
+        {/* 회원가입 버튼 */}
+        <div
+          onClick={signupClick}
+          className={`${styles.materialbutton} ${styles.alt2}`}
+        >
+          <span className={styles.shape}></span>
+          <span className={styles.textshape}>회원가입</span>
+        </div>
       </div>
     </div>
   )
